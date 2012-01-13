570af859dbe1c721e543a1e4be947642616b8fe4
Jeff Guo <gwjjeff@gmail.com>
2012-01-13 14:10:56 +0800

    PBKDF2.js

--- crypto-js-2.x/src/PBKDF2.js
+++ crypto-js-2.x/src/PBKDF2.js
@@ -1,8 +1,7 @@
-(function(){
+(function(C){
 
 // Shortcuts
-var C = Crypto,
-    util = C.util,
+var util = C.util,
     charenc = C.charenc,
     UTF8 = charenc.UTF8,
     Binary = charenc.Binary;
@@ -45,4 +44,4 @@ C.PBKDF2 = function (password, salt, keylen, options) {
 
 };
 
-})();
+})(Crypto);
