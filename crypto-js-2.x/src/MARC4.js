c810efdc71c2c9039cff33dd20f917a5019d00e7
Jeff Guo <gwjjeff@gmail.com>
2012-01-13 14:04:56 +0800

    MARC4.js

--- crypto-js-2.x/src/MARC4.js
+++ crypto-js-2.x/src/MARC4.js
@@ -1,8 +1,7 @@
-(function(){
+(function(C){
 
 // Shortcuts
-var C = Crypto,
-    util = C.util,
+var util = C.util,
     charenc = C.charenc,
     UTF8 = charenc.UTF8,
     Binary = charenc.Binary;
@@ -113,4 +112,4 @@ var MARC4 = C.MARC4 = {
 
 };
 
-})();
+})(Crypto);
